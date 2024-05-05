16f1f8fbf778babfb8d1df81a67e442f0d79d701
Coontzy1 <48108269+Coontzy1@users.noreply.github.com>
2024-05-05 17:32:31 -0400

    Delete Powershell/ADUsers_Export.ps1

--- Powershell/ADUsers_Export.ps1
+++
@@ -1,5 +0,0 @@
-Write-Output test
-# Path: testing.ps1
-# Gathering all propeties of all users in Active Directory
-# Exporting to .CSV file
-Get-ADUser -Filter * -Properties * | Export-Csv -PATH C:\Temp\ADUsers.csv -NoTypeInformation
