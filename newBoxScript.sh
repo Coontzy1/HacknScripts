a402245c2a5b12a8491ada6b45d2edc56357d598
Austin Coontz <48108269+Coontzy1@users.noreply.github.com>
2024-05-11 19:35:16 -0400

    Update newBoxScript.sh - Timing delay fixes

--- newBoxScript.sh
+++ newBoxScript.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # :triangle: WARNING - This uses rustscan. If you do not have it gg go next
 # This script was made to run in Kali using qterminal -- Meaning it uses default wordlists and qterminal commands with xdotool
+# Seems to be a problem with web shit and probably need to make a delay between things..........
+
 
 # COLORS
 RED='\033[0;31m'
@@ -22,17 +24,19 @@ main() {
     f_rename_tab "SCANNING"
 
     f_split_vertically
-    sleep 0.5
+    sleep 2.5
     xdotool type --delay 15 "sudo nmap -sU $IP -F"; xdotool key Return
     xdotool type --delay 15 --clearmodifiers "$sudoPassword"; xdotool key Return
     
     sleep 15
     if grep -q "Open $IP:445$" RUSTSCAN.OUTPUT123 ; then f_smb_enum; fi #SMB 
     if grep -q "Open $IP:21$" RUSTSCAN.OUTPUT123 ; then f_ftp_enum; fi #FTP
-    if grep -q "Open $IP:80$" RUSTSCAN.OUTPUT123 ; then f_http_enum 80; fi #HTTP
-    if grep -q "Open $IP:443$" RUSTSCAN.OUTPUT123 ; then f_http_enum 443; fi #HTTP
-    if grep -q "Open $IP:8080$" RUSTSCAN.OUTPUT123 ; then f_http_enum 8080; fi #HTTP
-    if grep -q "Open $IP:8000$" RUSTSCAN.OUTPUT123 ; then f_http_enum 8000; fi #HTTP
+
+    # These need some type of delay in between because runs too quickly
+    if grep -q "Open $IP:80$" RUSTSCAN.OUTPUT123 ; then sleep 5 && f_http_enum 80; fi #HTTP
+    if grep -q "Open $IP:443$" RUSTSCAN.OUTPUT123 ; then sleep 5 && f_http_enum 443; fi #HTTP
+    if grep -q "Open $IP:8080$" RUSTSCAN.OUTPUT123 ; then sleep 5 &&  f_http_enum 8080; fi #HTTP
+    if grep -q "Open $IP:8000$" RUSTSCAN.OUTPUT123 ; then sleep 5 &&  f_http_enum 8000; fi #HTTP
     
     
 
@@ -115,7 +119,7 @@ f_rename_tab() {
 f_smb_enum() {
     f_print_blue "Running SMB Enumeration..."
     xdotool key Ctrl+Shift+T
-    xdotool type --delay 15 "echo -e '\033[1;31mANON ACCESS NETEXEC\033[0m' && netexec smb $IP -u '' -p '' && echo -e '\033[1;31mSMB SHARES NETEXEC\033[0m' && netexec smb $IP --shares -u '' -p '' && echo -e '\033[1;31mENUM4LINUX\033[0m' && enum4linux-ng $IP -A"
+    xdotool type --delay 15 "echo -e '\033[1;31mANON ACCESS NETEXEC\033[0m' && netexec smb $IP -u '' -p '' && echo -e '\033[1;31mSMB SHARES NETEXEC\033[0m' && netexec smb $IP --shares -u '' -p '' && echo -e '\033[1;31mENUM4LINUX\033[0m' && enum4linux-ng $IP -A -C "
     xdotool key Return
     f_rename_tab "SMB :D"
 }
